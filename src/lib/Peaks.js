460d638a7dc32979092e4739077d37cd51b206ba
sound-ws <mark@firstcoders.co.uk>
2024-02-29 10:02:59 +0000

    Update from https://github.com/firstcoders/monorepo/commit/b2ea793623e1693654adbb9c5b7929424c190b00

--- src/lib/Peaks.js
+++ src/lib/Peaks.js
@@ -14,6 +14,33 @@ export default class Peaks {
     }
   }
 
+  /**
+   * Combines multiple Peaks into one
+   * @param  {...any} peakss
+   * @returns
+   */
+  static combine(...peakss) {
+    const arrays = peakss.map(p => p.data);
+
+    const n = arrays.reduce((max, xs) => Math.max(max, xs.length), 0);
+    const result = Array.from({ length: n });
+
+    const data = result.map((_, i) =>
+      arrays
+        .map(xs => xs[i] || 0)
+        .reduce((sum, x) => {
+          const f = Math.sqrt(x * x);
+          const g = Math.sqrt(sum * sum);
+
+          return f > g ? x : sum;
+        }, 0),
+    );
+
+    // for the moment, we assume all peaks have the same scale, same duration, same samples per second
+    // TODO implement resampling
+    return new Peaks({ ...peakss[0], data });
+  }
+
   get peaksPerSecond() {
     return this.data.length / this.duration;
   }
